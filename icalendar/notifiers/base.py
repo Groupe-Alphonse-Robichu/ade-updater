c9848c97b16968ec951dbcef43d0516b2f62906a
Nathan PERIER <periernathan2000@gmail.com>
2022-01-28 10:39:44 +0100

    Changed the contract of the base notifier

--- icalendar/notifiers/base.py
+++ icalendar/notifiers/base.py
@@ -1,14 +1,17 @@
 
+from icalendar.utils.parser import CalendarObject
+
+
 class BaseNotifier :
 
-	def missingTranslation(self, channel, summaries) :
+	def missingTranslation(self, channel, group_name, summaries) :
 		raise NotImplementedError
 
-	def discovered(self, channel, event_counts) :
+	def discovered(self, channel, cal_name, event_counts) :
 		raise NotImplementedError
 	
-	def weekSchedule(self, channel, events) :
+	def weekSchedule(self, channel, cal_name, events: "list[CalendarObject]") :
 		raise NotImplementedError
 	
-	def changes(self, channel, insertions, deletions, modifications) :
+	def changes(self, channel, cal_name, insertions, deletions, modifications) :
 		raise NotImplementedError
