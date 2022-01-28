a99ead6a3b7b18fd71b249ac5c8c4b2058dac5d7
Nathan PERIER <periernathan2000@gmail.com>
2022-01-28 11:51:18 +0100

    Added saving of missing translations

--- icalendar/group.py
+++ icalendar/group.py
@@ -1,5 +1,6 @@
 
 from icalendar.cal import CalendarConf
+from icalendar.notifiers.base import BaseNotifier
 
 
 class GroupConf :
@@ -17,9 +18,10 @@ class GroupConf :
 	def getCalendar(self, cal_name) :
 		return CalendarConf(cal_name, self._name, self._group)
 	
-	def forEach(self, func, states=None) -> "tuple[bool, bool]" :
+	def forEach(self, func, notifier: BaseNotifier, states=None) -> "tuple[bool, bool]" :
 		save_conf = False
 		save_states = False
+		no_translate = set()
 		
 		for cal_name in self._group['calendars'] :
 			cal = self.getCalendar(cal_name)
@@ -29,8 +31,15 @@ class GroupConf :
 				cal_states = states[cal_name]
 			else :
 				cal_states = None
-			sc, ss = func(cal, cal_states)
+			sc, ss, nt = func(cal, notifier, cal_states)
 			save_conf = save_conf or sc
 			save_states = save_states or ss
+			no_translate.update(nt)
+
+		if len(no_translate) > 0 :
+			save_conf = True
+			for elt in no_translate :
+				self._group['translate'][elt] = None
+			notifier.missingTranslation(self._group['alert'], self._name, no_translate)
 
 		return save_conf, save_states 
