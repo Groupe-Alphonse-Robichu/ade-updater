9a207a6d4b98f0330ce9b6eaef6b2f47a0f112ce
Nathan PERIER <periernathan2000@gmail.com>
2022-02-05 16:03:41 +0100

    Made the notifier display discover data in the terminal by default (very big tables)

--- icalendar/notifiers/base.py
+++ icalendar/notifiers/base.py
@@ -1,7 +1,9 @@
 
 from icalendar.cal import CalendarConf
 from icalendar.group import GroupConf
+from icalendar.utils.date import AdeDate, formatTimedelta
 from icalendar.utils.parser import CalendarObject
+from icalendar.utils.table import printTable
 
 
 class BaseNotifier :
@@ -12,10 +14,19 @@ class BaseNotifier :
 	def archive(self, group: GroupConf) :
 		raise NotImplementedError
 
-	def discovered(self, cal: CalendarConf, event_counts, begin, end) :
-		raise NotImplementedError
+	def discovered(self, cal: CalendarConf, event_counts: dict, begin, end) :
+		columns = ['Event', 'Count', 'Duration']
+		ll = [(evt, c[0], formatTimedelta(c[1])) for evt, c in event_counts.items()]
+		total_nb = 0
+		total_time = 0.0
+		for c in event_counts.values() :
+			total_nb += c[0]
+			total_time += c[1]
+		ll.insert(0, columns)
+		ll.append(('Total', total_nb, formatTimedelta(total_time)))
+		printTable(ll, f"{cal.getFullName()} {AdeDate(begin).format()} - {AdeDate(end).format()}")
 	
-	def weekSchedule(self, cal: CalendarConf, ical: CalendarObject) :
+	def weekSchedule(self, cal: CalendarConf, ical: CalendarObject, startOfWeek: AdeDate) :
 		raise NotImplementedError
 	
 	def changes(self, cal: CalendarConf, insertions, deletions, modifications) :
--- icalendar/notifiers/terminal.py
+++ icalendar/notifiers/terminal.py
@@ -4,7 +4,7 @@ from icalendar.group import GroupConf
 from icalendar.notifiers.base import BaseNotifier
 from icalendar.utils.table import printTable
 from icalendar.utils.parser import CalendarObject
-from icalendar.utils.date import formatDatetime, formatTimedelta
+from icalendar.utils.date import AdeDate, IcalDate, formatTimedelta
 
 import logging
 
@@ -19,27 +19,15 @@ class TerminalNotifier(BaseNotifier) :
 	
 	def archive(self, group: GroupConf) :
 		pass
-
-	def discovered(self, cal: CalendarConf, event_counts: dict, begin, end) :
-		columns = ['Event', 'Count', 'Duration']
-		ll = [(evt, c[0], formatTimedelta(c[1])) for evt, c in event_counts.items()]
-		total_nb = 0
-		total_time = 0.0
-		for c in event_counts.values() :
-			total_nb += c[0]
-			total_time += c[1]
-		ll.insert(0, columns)
-		ll.append(('Total', total_nb, formatTimedelta(total_time)))
-		printTable(ll, cal.getFullName())
 	
-	def weekSchedule(self, cal: CalendarConf, ical: CalendarObject) :
+	def weekSchedule(self, cal: CalendarConf, ical: CalendarObject, startOfWeek: AdeDate) :
 		columns = ['Time', 'Event', 'Location']
 		ll = [
 			(
-				formatDatetime(evt.getProperty('DTSTART')),
+				str(IcalDate(evt.getProperty('DTSTART'))),
 				evt.getProperty('SUMMARY'),
 				evt.getPropertyOrDefault('LOCATION')
-			) for evt in ical.getObjects()
+			) for evt in ical
 		]
 		ll.insert(0, columns)
 		printTable(ll, f"{cal.getFullName()} - week {cal.getWeek()}")
