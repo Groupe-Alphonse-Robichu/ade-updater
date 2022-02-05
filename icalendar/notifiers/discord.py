1a6eafb37dd7f53107b6d4581d20768e0f8dd1a3
Nathan PERIER <periernathan2000@gmail.com>
2022-02-05 01:03:26 +0100

    Added a base for the Discord notifier

---
+++ icalendar/notifiers/discord.py
@@ -0,0 +1,18 @@
+
+from icalendar.notifiers.base import BaseNotifier
+from icalendar.utils.parser import CalendarObject
+
+
+class DiscordNotifier(BaseNotifier) :
+
+	def missingTranslation(self, channel, group_name, summaries) :
+		raise NotImplementedError
+
+	def discovered(self, channel, cal_name, event_counts) :
+		raise NotImplementedError
+	
+	def weekSchedule(self, channel, cal_name, week, ical: CalendarObject) :
+		raise NotImplementedError
+	
+	def changes(self, channel, cal_name, insertions, deletions, modifications) :
+		raise NotImplementedError
