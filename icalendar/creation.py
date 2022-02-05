58bd19dd38fd8e888146e20be176bcd5e0257afa
Nathan PERIER <periernathan2000@gmail.com>
2022-02-05 01:00:20 +0100

    Added utility to create configuration easily

---
+++ icalendar/creation.py
@@ -0,0 +1,47 @@
+from icalendar import loadJson, saveJson, CONF_FILE
+from icalendar.utils.date import AdeDate
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def createCalendarGroup(name: str) -> bool :
+	data = loadJson(CONF_FILE)
+	if name in data :
+		logger.warning(f"EXISTS calendar group {name}")
+		return False
+	data[name] = {
+		'source': None,
+		'project_id': None,
+		'resources': [],
+		'dest_folder': None,
+		'start': str(AdeDate.today),
+		'limit': None,
+		'alert': None,
+		'ignore': [],
+		'translate': {},
+		'calendars': {}
+	}
+	saveJson(data, CONF_FILE)
+	logger.info(f"CREATED calendar group {name}")
+	return True
+
+
+def createCalendar(group: str, name: str) -> bool :
+	data = loadJson(CONF_FILE)
+	if group not in data :
+		logger.warning(f"NOT_FOUND calendar group {group}")
+		return False
+	if name in data[group] :
+		logger.warning(f"EXISTS calendar {name} in calendar group {group}")
+		return False
+	data[group][name] = {
+		'resources': [],
+		'notify': None,
+		'update': 'never',
+		'week': ''
+	}
+	saveJson(data, CONF_FILE)
+	logger.info(f"CREATED calendar {name} in calendar group {group}")
+	return True
