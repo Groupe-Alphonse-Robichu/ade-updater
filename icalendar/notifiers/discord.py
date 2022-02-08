1fcd553d950fa64892067d8c7b7c22ad15cfd825
Nathan PERIER <periernathan2000@gmail.com>
2022-02-08 09:41:38 +0100

    Added details for calendar modifications

--- icalendar/notifiers/discord.py
+++ icalendar/notifiers/discord.py
@@ -1,5 +1,4 @@
 
-from importlib.resources import contents
 from icalendar.cal import CalendarConf
 from icalendar.group import GroupConf
 from icalendar.notifiers.base import BaseNotifier
@@ -18,6 +17,10 @@ PROJECT_URL = "https://github.com/Groupe-Alphonse-Robichu/ade-updater"
 def formatRole(role_id) :
 	return f"<@&{role_id}>"
 
+def formatTimeRange(evt) :
+	date, time1 = IcalDate(evt[0]).splitDatetime()
+	time2 = IcalDate(evt[1]).formatTime()
+	return date, f"{time1} - {time2}"
 
 # The notifier may start to spam Discord servers a little when there are a lot of calendars.
 # Maybe add a delay betwenn each request ?
@@ -87,9 +90,8 @@ class DiscordNotifier(BaseNotifier) :
 			logger.warning(f"UNDEFINED alert channel for calendar {cal.getFullName()}")
 			return
 		edits = {
-			"Nouveaux cours": ('3bf573', insertions),
-			"Cours supprimés": ('f53b3b', deletions),
-			"Cours modifiés": ('3b9bf5', [m[1] for m in modifications])
+			'Nouveaux cours': ('3bf573', insertions),
+			'Cours supprimés': ('f53b3b', deletions)
 		}
 		webhook = DiscordWebhook(url=cal.getNotify())
 		message = f"Des modifications ont été détectées depuis le {AdeDate.fromString(cal.getUpdate()).format()}"
@@ -103,13 +105,38 @@ class DiscordNotifier(BaseNotifier) :
 			if len(evt_states) > 0 :
 				embed = DiscordEmbed(title=title, color=content[0])
 				for evt in evt_states :
-					date, time1 = IcalDate(evt[0]).splitDatetime()
-					time2 = IcalDate(evt[1]).formatTime()
-					desc = f"{date}, {time1} - {time2}" + (f"\n{evt[3]}" if len(evt[3]) > 0 else "")
+					date, time = formatTimeRange(evt)
+					desc = f"{date}, {time}"
+					if len(evt[3]) > 0 :
+						desc += f"\n{evt[3]}"
 					embed.add_embed_field(name=evt[2], value=desc)
 				embed.set_footer(text=PROJECT_URL)
 				embed.set_timestamp()
 				webhook.add_embed(embed)
+		if len(modifications) > 0 :
+			embed = DiscordEmbed(title='Cours modifiés', color='3b9bf5')
+			for old_state, new_state in modifications :
+				title = f"~~{old_state[2]}~~ -> **{new_state[2]}**" if old_state[2] != new_state[2] else old_state[2]
+				d1, t1 = formatTimeRange(old_state)
+				d2, t2 = formatTimeRange(new_state)
+				date = f"~~{d1}~~ -> **{d2}**" if d1 != d2 else d1
+				time = f"~~{t1}~~ -> **{t2}**" if t1 != t2 else t1
+				desc = f"{date}\n{time}"
+				if old_state[3] != '' or new_state[3] != '' :
+					desc += "\n"
+					if old_state[3] != new_state[3] :
+						if old_state[3] == '' :
+							desc += f"**{new_state[3]}**"
+						elif new_state[3] == '' :
+							desc += f"~~{old_state[3]}~~"
+						else :
+							desc += f"~~{old_state[3]}~~ -> **{new_state[3]}**"
+					else :
+						desc += old_state[3]
+				embed.add_embed_field(name=title, value=desc)
+			embed.set_footer(text=PROJECT_URL)
+			embed.set_timestamp()
+			webhook.add_embed(embed)
 		response = webhook.execute()
 		if response.ok :
 			logger.info(f"WEBHOOK posted for calendar {cal.getFullName()}")
