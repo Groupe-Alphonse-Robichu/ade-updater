bb43fea0fe68b7bb3fa5bf40039bb3453552d98b
Nathan PERIER <periernathan2000@gmail.com>
2022-08-28 11:24:38 +0200

    Fixed typing in base source object

--- sources/base.py
+++ sources/base.py
@@ -1,13 +1,12 @@
 
+from icalendar.utils.date import AdeDate
 from icalendar.utils.parser import CalendarObject
-
-from datetime import date
 
 
 class CalendarSource :
 
-	def getURL(self, begin: date, end: date) -> str :
+	def getURL(self, begin: AdeDate, end: AdeDate) -> str :
 		raise NotImplementedError('Subtypes of CalendarSource must implement the getURL method')
 
-	def processEvent(self, event: CalendarObject) -> CalendarObject :
+	def processEvent(self, event: CalendarObject) -> "CalendarObject | None" :
 		return event
